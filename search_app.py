788c38b022e99703312dad16ec55dfc4a4eb460c
briannaeskin <briannaeskin@yahoo.com>
2022-04-24 17:03:56 -0400

    Handle retweets properly, search by user function

--- search_app.py
+++ search_app.py
@@ -5,6 +5,7 @@ Function: Holds the search app, including the GUI
 
 from pymongo import MongoClient
 import mysql.connector as connection
+import time
 
 class Search:
 
@@ -17,11 +18,90 @@ class Search:
         self.mysql_connection = connection.connect(host="localhost", user="data_man_group3", passwd="abc123",
                                               database="twitter_users")
         self.mysql_connection.autocommit = True
-        self.cursor = mysql_connection.cursor(prepared=True)
+        self.cursor = self.mysql_connection.cursor(prepared=True)
 
         #MongoDB Connection
         self.client = MongoClient("mongodb+srv://data_man_group3:dkncO5uFyOd6IB5Y@cluster0.na33p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-        self.twitter_data = client["twitter_data"]
-        self.tweet_info_collection = twitter_data["tweet_info"]
+        self.twitter_data = self.client["twitter_data"]
+        self.tweet_info_collection = self.twitter_data["tweet_info"]
 
         #Placeholder for Redis store
+
+    def search_by_user(self, user_name, timerange_lower=None, timerange_upper=None):
+        """
+        Given a user name, searches for relevant information about user, including name, screen_name, number of followers,
+        number of friends, total number of tweets (including retweets), their most recent tweet (including retweets), and
+        their 5 most popular tweets (or all tweets if user tweeted less than 5 times)
+        """
+
+        redis_enabled = False #Temp variable since no cache is set up yet. Will remove this and all reference once set up
+
+        #Query user_id based on screen_name, required for Redis lookup, so needs to execute regardless
+        self.cursor.execute("""SELECT * FROM user_info WHERE screen_name = '{}'""".format(user_name))
+        user = self.cursor.fetchone()
+
+        if not user:#No user found
+            message = "No user found with provided user name"
+            return message
+
+        user_id = user[0]
+
+        start_time = time.time()
+
+        if redis_enabled:
+            """
+            This is the placeholder for the cache check. For now passing. But when ready for implementation will need to
+            replace with check for redis key and fill in accordingly
+            """
+            cache_status = "Found in Redis"
+            pass
+        else:
+            cache_status = "Not Found in Redis. Data pulled from Mongo and mySQL"
+            user_name = user[1]
+            screen_name = user[2]
+            num_followers = user[3]
+            num_friends = user[4]
+
+            tweets = self.tweet_info_collection.find({'user_id': user_id}).sort('total_engagement', -1)
+
+            num_tweets = 0
+            most_recent_tweet = None
+            popular_tweets = ""
+
+            for tweet in tweets:
+                num_tweets += 1
+                if num_tweets <= 5:
+                    popular_tweets += str(tweet) + '\n'
+                try:
+                    if tweet['tweeted_date'] > most_recent_tweet['tweeted_date']:
+                        most_recent_tweet = tweet
+                except:
+                    most_recent_tweet = tweet
+
+            most_recent_tweet_str = str(most_recent_tweet)
+            end_time = time.time()
+            query_runtime = end_time - start_time
+
+            query_runtime_ms = query_runtime*1000
+            runtime = "Results returned in: {} ms".format(query_runtime_ms)
+
+            result = """
+            Result for user name: {}
+        
+            Name: {}
+            Number of Followers: {}
+            Number of Friends: {}
+            Total Tweets (Including Retweets): {}
+            Most Recent Tweet: {}
+        
+            Top Tweets: {}
+            """.format(screen_name, user_name, num_followers, num_friends, num_tweets, most_recent_tweet_str, popular_tweets)
+
+            #Add to Redis cache
+
+            return cache_status + '\n' + runtime + '\n' + result
+
+if __name__ == "__main__":
+    search = Search()
+    print(search.search_by_user("biannagolodryga"))
+
--- store_twitter_data.py
+++ store_twitter_data.py
@@ -23,10 +23,9 @@ def setup_mysql():
     screen_name VARCHAR(255),
     followers_count INT,
     friends_count INT,
-    num_tweets INT,
     last_tweet_id VARCHAR(255),
     PRIMARY KEY(user_id),
-    INDEX(screen_name, followers_count, friends_count, num_tweets)
+    INDEX(screen_name, followers_count, friends_count)
     );
     """
 
@@ -62,8 +61,7 @@ def mongo_indexes(twitter_data):
     twitter_data.tweet_info.create_index("tweet_id")
     twitter_data.tweet_info.create_index("created_date")
     twitter_data.tweet_info.create_index("user_id")
-    twitter_data.tweet_info.create_index("favorite_count")
-    twitter_data.tweet_info.create_index("retweet_count")
+    twitter_data.tweet_info.create_index("total_engagement")
     twitter_data.tweet_info.create_index("rt_id")
 
 def insert_mysql(data, connection, cursor, insert):
@@ -72,36 +70,47 @@ def insert_mysql(data, connection, cursor, insert):
     """
     INSERT_QUERY = """
     INSERT INTO user_info (
-    user_id, name, screen_name, followers_count, friends_count, num_tweets, last_tweet_id
+    user_id, name, screen_name, followers_count, friends_count, last_tweet_id
     )
     VALUES (
-    '{}','{}','{}',{},{},{},'{}'
+    '{}','{}','{}',{},{},'{}'
     );
-    """.format(data[0], data[1], data[2], data[3], data[4], data[5], data[6])
+    """.format(data[0], data[1], data[2], data[3], data[4], data[5])
 
     UPDATE_QUERY = """
-    UPDATE user_info SET name = '{}', screen_name = '{}', followers_count = {}, friends_count = {}, num_tweets = {},
+    UPDATE user_info SET name = '{}', screen_name = '{}', followers_count = {}, friends_count = {},
     last_tweet_id = '{}' WHERE user_id = '{}';
-    """.format(data[1], data[2], data[3], data[4], data[5], data[6], data[0])
+    """.format(data[1], data[2], data[3], data[4], data[5], data[0])
 
     if insert:
         cursor.execute(INSERT_QUERY)
+        print("User ", data[2], "inserted")
     else:
         cursor.execute(UPDATE_QUERY)
         connection.commit()
-
+        print("User", data[2], "updated")
 
 def insert_mongo(data, twitter_data):
     """
     Insert document to MongoDB. Already know this is new since we check if exists in main method
     """
     twitter_data.tweet_info.insert_one(data)
+    print("Tweet inserted")
+
+def update_mongo(twitter_data, id_str, favorite_count, retweet_count, total_engagement):
+    myquery = {"tweet_id": id_str}
+    newvalues = {"$set": {
+        "favorite_count": favorite_count,
+        "retweet_count": retweet_count,
+        "total_engagement": total_engagement
+    }}
+    twitter_data.tweet_info.update_one(myquery, newvalues)
+    print("Tweet updated in Mongo")
 
 def convert_time_created(created_at):
     """
     Convert created_at string to datetime that can be used for search purposes
     """
-    #print("Starting time convert")
     time_list = created_at.split()
     month = time_list[1]
     day = time_list[2]
@@ -137,93 +146,116 @@ def convert_time_created(created_at):
     elif month == "Dec":
         month = 12
 
-    #print("Ready to call datetime function")
     new_date = datetime(year = int(year), month = month, day = int(day), hour = int(hour), minute = int(min),
                         second = int(sec), tzinfo = timezone.utc)
-    #print("Finished with time convert")
     return new_date
 
+def is_tweet_new(id_str, twitter_data):
+    doc = twitter_data.tweet_info.find_one({'tweet_id': id_str})
+    if doc:
+        return False
+    else:
+        return True
+
+def gather_user_info(user, data, id_set):
+    user_id = user['id_str']
+    id_set.add(user_id)
+    name = user['name']
+    # remove "'" from name since causes issues in mySQL insert
+    name = re.sub("'", "", name)
+    screen_name = user['screen_name']
+    followers_count = user['followers_count']
+    friends_count = user['friends_count']
+    last_tweet = data['id_str']
+    return user_id, name, screen_name, followers_count, friends_count, last_tweet, id_set
+
+def gather_tweet_info(data):
+    hashtags = []
+    is_retweet = False
+    text = data['text']
+    favorite_count = data['favorite_count'] if data['favorite_count'] else 0
+    retweet_count = data['retweet_count'] if data['retweet_count'] else 0
+    total_engagement = favorite_count + retweet_count
+    for hashtag in data['entities']['hashtags']:
+        hashtags.append(hashtag['text'])
+    retweet_hashtags = None
+    retweet_text = None
+    retweet_id = None
+    return hashtags, is_retweet, text, favorite_count, retweet_count, total_engagement, retweet_hashtags, retweet_text, retweet_id
+
+def gather_retweet_info(data):
+    is_retweet = True
+    retweet_hashtags = []
+    for hashtag in data['entities']['hashtags']:
+        retweet_hashtags.append(hashtag['text'])
+    retweet_text = data['text']
+    retweet_id = data['id_str']
+    return is_retweet, retweet_hashtags, retweet_text, retweet_id
 
 if __name__ == "__main__":
     mysql_connection, mysql_cursor = setup_mysql()
     twitter_data = setup_mongo()
 
     id_set = set()
+    tweet_id_set = set()
 
     with open("corona-out-2.json", "r") as f1:
         for line in f1:
             try:
                 data = json.loads(line)
                 print("Starting for ID: ", data['id_str'])
+                tweet_id_set.add(data['id_str'])
 
                 #Is tweet new
-                if twitter_data.tweet_info.find_one({'id_str' : data['id_str']}):
-                    print("Skipping tweet ", data['id_str'], " since already exists in DB")
+                if not is_tweet_new(data['id_str'], twitter_data):
+                    print("Skipping tweet", data['id_str'], "since it already exists in DB")
                     continue
+
+                print("Starting user checks")
 
                 user = data['user']
 
-                #print("Starting user extract")
-                #Extract User Info for DB
-                user_id = user['id_str']
-                id_set.add(user_id)
-                name = user['name']
-                #remove "'" from name since causes issues in mySQL insert
-                name = re.sub("'", "", name)
-                screen_name = user['screen_name']
-                followers_count = user['followers_count']
-                friends_count = user['friends_count']
-                last_tweet = data['id_str']
-                #print("Finished user extract")
+                user_id, name, screen_name, followers_count, friends_count, last_tweet, id_set = gather_user_info(user, data, id_set)
 
                 mysql_cursor.execute("SELECT * FROM user_info where user_id = %s", (user_id,))
                 user_entry = mysql_cursor.fetchone()
                 if not user_entry: #Entry not in table, need to add
-                    print("Insert new user ", screen_name)
-                    insert_data = [user_id, name, screen_name, followers_count, friends_count, 1, last_tweet]
+                    print("Insert new user", screen_name)
+                    insert_data = [user_id, name, screen_name, followers_count, friends_count, last_tweet]
                     insert_mysql(insert_data, mysql_connection, mysql_cursor, insert = True)
-                    #print("User inserted")
                 else: #User exists
-                    num_tweets = user_entry[5] + 1
-                    if data['id_str'] > user_entry[6]: #More recent tweet, so check that other info is up to date
-                        update_data = [user_id, name, screen_name, followers_count, friends_count, num_tweets, last_tweet]
+                    print("Last Tweet: ", user_entry[5])
+                    if data['id_str'] > user_entry[5]: #More recent tweet, so check that other info is up to date
+                        print("Update user", screen_name)
+                        update_data = [user_id, name, screen_name, followers_count, friends_count, last_tweet]
                         insert_mysql(update_data, mysql_connection, mysql_cursor, insert = False)
-                    else: #Less recent tweet, update num tweets only
-                        update_data = [user_id, user_entry[1], user_entry[2], user_entry[3], user_entry[4], num_tweets, user_entry[6]]
-                        insert_mysql(update_data, mysql_connection, mysql_cursor, insert = False)
-                    print("User", screen_name, "updated")
+                    else:
+                        print("Tweet more recent, skipping update")
 
-                #print("Compiling tweet info")
-                hashtags = []
-                is_retweet = False
-                text = data['text']
-                for hashtag in data['entities']['hashtags']:
-                    hashtags.append(hashtag['text'])
-                retweet_hashtags = None
-                retweet_text = None
-                retweet_id = None
-                #print("Checking if retweet")
-                if (data['text'].startswith('RT')):
-                    print(data['id_str'], "is retweet")
-                    is_retweet = True
+                print("Start compiling tweet info")
+
+                hashtags, is_retweet, text, favorite_count, retweet_count, total_engagement, retweet_hashtags, retweet_text, retweet_id = gather_tweet_info(data)
+
+                if data['text'].startswith('RT'):
+                    print("Is retweet")
                     try:
-                        retweet_hashtags = []
-                        for hashtag in data['retweeted_status']['entities']['hashtags']:
-                            retweet_hashtags.append(hashtag['text'])
-                        retweet_text = data['retweeted_status']['text']
-                        retweet_id = data['retweeted_status']['id_str']
+                        retweet_data = data['retweeted_status']
+                        is_retweet, retweet_hashtags, retweet_text, retweet_id = gather_retweet_info(retweet_data)
                     except:
-                        #print("No retweeted_status")
+                        print("No retweeted status")
+                        is_retweet = True
                         retweet_hashtags = None
                         retweet_text = None
                         retweet_id = None
-                #print("Making tweet document for storage")
+
+
                 tweet = {
                     "tweet_id": data['id_str'],
                     "tweeted_date": convert_time_created(data['created_at']),
                     "user_id": user_id,
-                    "favorite_count": data['favorite_count'],
-                    "retweet_count": data['retweet_count'],
+                    "favorite_count": favorite_count,
+                    "retweet_count": retweet_count,
+                    "total_engagement": total_engagement,
                     "is_retweet": is_retweet,
                     "orig_hashtags": hashtags,
                     "rt_hashtags": retweet_hashtags,
@@ -231,17 +263,83 @@ if __name__ == "__main__":
                     "rt_text": retweet_text,
                     "rt_id": retweet_id
                 }
-                #print("Tweet setup, ready to insert")
+
                 insert_mongo(tweet, twitter_data)
-                #print("Finished for ID: ", data['id_str'])
+
+                if retweet_data:
+                    print("Need to write retweeted tweet to database")
+                    print("Original tweet ID:", retweet_data['id_str'])
+                    tweet_id_set.add(retweet_data['id_str'])
+
+                    if is_tweet_new(retweet_data['id_str'], twitter_data):
+                        print("Original tweet", retweet_data['id_str'], "is new")
+                        is_orig_tweet_new = True
+                    else:
+                        print("Original tweet is already in DB")
+                        is_orig_tweet_new = False
+
+                    print("Starting original tweet user checks")
+
+                    orig_user = retweet_data['user']
+
+                    orig_user_id, orig_name, orig_screen_name, orig_followers_count, orig_friends_count, orig_last_tweet, id_set = gather_user_info(orig_user, data, id_set)
+
+                    mysql_cursor.execute("SELECT * FROM user_info where user_id = %s", (orig_user_id,))
+                    orig_user_entry = mysql_cursor.fetchone()
+                    if not orig_user_entry: #Entry not in table, need to add
+                        print("Insert new original user", orig_screen_name)
+                        insert_orig_data = [orig_user_id, orig_name, orig_screen_name, orig_followers_count, orig_friends_count, last_tweet]
+                        insert_mysql(insert_orig_data, mysql_connection, mysql_cursor, insert = True)
+                    else: #Original User Exists
+                        print("Last tweet:", orig_user_entry[5])
+                        if last_tweet > orig_user_entry[5]: #More recent tweet, so check that other info is up to date
+                            print("Update original user", screen_name)
+                            orig_update_data = [orig_user_id, orig_name, orig_screen_name, orig_followers_count, orig_friends_count, last_tweet]
+                            insert_mysql(orig_update_data, mysql_connection, mysql_cursor, insert = False)
+                        else:
+                            print("More recent tweet, skipping update")
+
+                    orig_hashtags, orig_is_retweet, orig_text, orig_favorite_count, orig_retweet_count, orig_total_engagement, orig_retweet_hashtags, orig_retweet_text, orig_retweet_id = gather_tweet_info(retweet_data)
+
+                    retweet = {
+                        "tweet_id": retweet_data['id_str'],
+                        "tweeted_date": convert_time_created(retweet_data['created_at']),
+                        "user_id": orig_user_id,
+                        "favorite_count": orig_favorite_count,
+                        "retweet_count": orig_retweet_count,
+                        "total_engagement": orig_total_engagement,
+                        "is_retweet": orig_is_retweet,
+                        "orig_hashtags": orig_hashtags,
+                        "rt_hashtags": orig_retweet_hashtags,
+                        "orig_text": orig_text,
+                        "rt_text": orig_retweet_text,
+                        "rt_id": orig_retweet_id
+                    }
+
+                    if is_orig_tweet_new:
+                        print("Original tweet is new, so inserting")
+                        insert_mongo(retweet, twitter_data)
+
+                    else:
+                        print("Tweet already exists, so updating")
+                        update_mongo(twitter_data, retweet_data['id_str'], orig_favorite_count, orig_retweet_count, orig_total_engagement)
+
+                print("Finished for ID: ", data['id_str'])
+
             except Exception as e:
                 print(e)
-                print("Hit exception for ID: ", data['id_str'])
-                #break
-                continue
-    #Create indexes
+                print("Hit exception for ID", data['id_str'])
+                break
+                #continue
+
+    #Create Indexes
     mongo_indexes(twitter_data)
+
+    #Checks
+    print("ID Set Size:", len(id_set))
+    print("Tweet ID Set Size:", len(tweet_id_set))
 
     #Close DB
     mysql_cursor.close()
     mysql_connection.close()
+
