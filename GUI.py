46bf872432a4fb1bca7a4266478e5ae6c55d907d
mgleyzer12 <52224880+mgleyzer12@users.noreply.github.com>
2022-04-29 18:47:18 -0400

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -12,6 +12,7 @@ from tkinter import *
 from tkinter.ttk import *
 from tkinter import ttk
 import json
+
 #Create an instance of Tkinter frame
 win = Tk()
 #Set the geometry of Tkinter frame
@@ -20,16 +21,16 @@ win.geometry("750x270")
 entry = StringVar()
 time_entry = StringVar()
 
+#User presses submit button and function returns the user's inputs 
 def submit():
     global entry
     global time_entry
-    
+    if time_entry.get() == "" : 
+        return entry.get() , ""
     time_range = json.loads(time_entry.get())
-    print(type(time_range))
-    print(entry.get())
-    print(time_range)
     return entry.get() , time_range
 
+# User selects option to input a tweet and query time range
 def open_popup_tweet():
    top= Toplevel(win)
    top.geometry("750x250")
@@ -45,7 +46,7 @@ def open_popup_tweet():
    
    ttk.Button(top,text = "Submit",command = submit).pack()
    
-   
+# User selects option to input a user and query time range   
 def open_popup_user(): 
    top= Toplevel(win)
    top.geometry("750x250")
@@ -60,6 +61,7 @@ def open_popup_user():
    t.pack()
    ttk.Button(top,text = "Submit",command = submit).pack()
 
+#User selects option to input a hashtag and query time range
 def open_popup_hashtag():
    top= Toplevel(win)
    top.geometry("750x250")
@@ -75,9 +77,9 @@ def open_popup_hashtag():
    
    ttk.Button(top,text = "Submit",command = submit).pack()
              
-#   Label(top, text= "Hello World!", font=('Mistral 18 bold')).place(x=150,y= 80)
    
 Label(win, text="What do you want to search for?", font=('Helvetica 14 bold')).pack(pady=20)
+
 #Create a button in the main Window to open the popup
 ttk.Button(win, text= "User", command= open_popup_user).pack()
 ttk.Button(win , text = "Tweet" , command=open_popup_tweet ).pack()
