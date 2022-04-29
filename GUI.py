a6c6d54d220f1e1d09b03840bae5fbb6ad5680be
mgleyzer12 <52224880+mgleyzer12@users.noreply.github.com>
2022-04-29 18:33:38 -0400

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -11,23 +11,38 @@ Created on Tue Apr 26 20:36:38 2022
 from tkinter import *
 from tkinter.ttk import *
 from tkinter import ttk
+import json
 #Create an instance of Tkinter frame
 win = Tk()
 #Set the geometry of Tkinter frame
 win.geometry("750x270")
 
 entry = StringVar()
+time_entry = StringVar()
+
 def submit():
     global entry
+    global time_entry
+    
+    time_range = json.loads(time_entry.get())
+    print(type(time_range))
     print(entry.get())
-    return entry.get()
+    print(time_range)
+    return entry.get() , time_range
 
 def open_popup_tweet():
    top= Toplevel(win)
    top.geometry("750x250")
    top.title("Tweet")
+   
+   Label(top, text="Input Tweet", font=('Helvetica 14 bold')).pack(pady=20)
    e = Entry(top, textvariable = entry)
    e.pack()
+   
+   Label(top, text="What time range do you want? (Format of input: [start,end])", font=('Helvetica 14 bold')).pack(pady=20)
+   t = Entry(top , textvariable = time_entry)
+   t.pack()
+   
    ttk.Button(top,text = "Submit",command = submit).pack()
    
    
@@ -35,25 +50,29 @@ def open_popup_user():
    top= Toplevel(win)
    top.geometry("750x250")
    top.title("User search")
+   
+   Label(top, text="Input User", font=('Helvetica 14 bold')).pack(pady=20)
    e = Entry(top, textvariable = entry)
    e.pack()
-   ttk.Button(top,text = "Submit",command = submit).pack()
    
-def open_popup_timerange(): 
-   top= Toplevel(win)
-   top.geometry("750x250")
-   top.title("time range")
-   searched_h = StringVar()  
-   e = Entry(top , textvariable = entry)
-   e.pack() 
+   Label(top, text="What time range do you want? (Format of input: [start,end])", font=('Helvetica 14 bold')).pack(pady=20)
+   t = Entry(top , textvariable = time_entry)
+   t.pack()
    ttk.Button(top,text = "Submit",command = submit).pack()
-   
+
 def open_popup_hashtag():
    top= Toplevel(win)
    top.geometry("750x250")
    top.title("hashtag search")
+   
+   Label(top, text="Input Hashtag", font=('Helvetica 14 bold')).pack(pady=20)
    e = Entry(top , textvariable = entry)
    e.pack() 
+   
+   Label(top, text="What time range do you want? (Format of input: [start,end])", font=('Helvetica 14 bold')).pack(pady=20)
+   t = Entry(top , textvariable = time_entry)
+   t.pack()
+   
    ttk.Button(top,text = "Submit",command = submit).pack()
              
 #   Label(top, text= "Hello World!", font=('Mistral 18 bold')).place(x=150,y= 80)
@@ -63,8 +82,4 @@ Label(win, text="What do you want to search for?", font=('Helvetica 14 bold')).p
 ttk.Button(win, text= "User", command= open_popup_user).pack()
 ttk.Button(win , text = "Tweet" , command=open_popup_tweet ).pack()
 ttk.Button(win, text = "Hashtag" , command=open_popup_hashtag).pack() 
-ttk.Button(win, text = "Time Range" , command = open_popup_timerange).pack()
 win.mainloop()
-
-if __name__ == "main" : 
-    submit()
